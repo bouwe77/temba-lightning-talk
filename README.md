ae46df3dd4ed8102b189b87ccec1fe0ac6c0af5b
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2023-07-24 22:04:05 +0200

    .

--- README.md
+++ README.md
@@ -7,8 +7,7 @@ This is a demo project for my talk about Temba, at WebinarGeek on July 28, 2023.
 ## Running the app
 
 ```
-export NODE_OPTIONS=--openssl-legacy-provider
-PORT=9227 elm-app start
+export NODE_OPTIONS=--openssl-legacy-provider && PORT=9227 elm-app start
 ```
 
 Of course, the actual port number does not matter.
--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,24 @@
 module Main exposing (..)
 
 import Browser
+import Html exposing (Html, button, div, h1, input, li, text, ul)
+import Html.Attributes exposing (checked, disabled, placeholder, type_, value)
+import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Decode as Decode
 import Json.Encode as Encode
-import Html exposing (Html, button, div, input, li, text, ul )
-import Html.Attributes exposing (placeholder, value, type_, checked, disabled)
-import Html.Events exposing (onClick)
-import Html.Events exposing (onInput)
+import Html exposing (p)
+
+
+todosUrl : String
+todosUrl =
+    "http://localhost:9228/todos"
+
+
 
 -- Helper function to convert HTTP errors to user-friendly strings
+
+
 errorToString : Http.Error -> String
 errorToString error =
     case error of
@@ -28,21 +37,26 @@ errorToString error =
         Http.BadBody message ->
             "Bad body: " ++ message
 
+
+
 -- MODEL
 
-type alias ToDo = 
+
+type alias ToDo =
     { id : String
     , title : String
     , completed : Bool
     }
 
-type alias Model = 
+
+type alias Model =
     { todos : List ToDo
     , input : String
     , editingId : Maybe String
     , editingTitle : Maybe String
     , error : Maybe String
     }
+
 
 init : Model
 init =
@@ -53,9 +67,12 @@ init =
     , error = Nothing
     }
 
+
+
 -- UPDATE
 
-type Msg 
+
+type Msg
     = GotTodos (Result Http.Error (List ToDo))
     | CreateToDo
     | NewInput String
@@ -65,82 +82,112 @@ type Msg
     | CancelEdit
     | DeleteToDo String
     | CompleteToDo String Bool
-    | NoOp
     | FetchTodos
     | HttpError Http.Error
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GotTodos result ->
             case result of
                 Ok todos ->
-                    ({ model | todos = todos, error = Nothing }, Cmd.none)
-                
+                    ( { model | todos = todos, error = Nothing }, Cmd.none )
+
                 Err error ->
-                    ({ model | error = Just (errorToString error) }, Cmd.none)
+                    ( { model | error = Just (errorToString error) }, Cmd.none )
 
         CreateToDo ->
-            ({ model | input = "" }, createToDo model.input)
+            ( { model | input = "" }, createToDo model.input )
 
         NewInput input ->
-            ({ model | input = input }, Cmd.none)
+            ( { model | input = input }, Cmd.none )
 
         EditToDo id ->
-            let 
-                maybeTodo = List.head (List.filter (\todo -> todo.id == id) model.todos)
+            let
+                maybeTodo =
+                    List.head (List.filter (\todo -> todo.id == id) model.todos)
             in
             case maybeTodo of
-                Just todo -> 
-                    ({ model | editingId = Just id, editingTitle = Just todo.title }, Cmd.none)
+                Just todo ->
+                    ( { model | editingId = Just id, editingTitle = Just todo.title }, Cmd.none )
 
-                Nothing -> 
-                    (model, Cmd.none)
+                Nothing ->
+                    ( model, Cmd.none )
 
         NewTitle title ->
-            ({ model | editingTitle = Just title }, Cmd.none)
+            ( { model | editingTitle = Just title }, Cmd.none )
 
         SaveEdit id ->
             case model.editingTitle of
                 Just title ->
-                    ({ model | editingId = Nothing, editingTitle = Nothing }, Cmd.batch [ editToDo id title, fetchTodos ])
+                    ( { model | editingId = Nothing, editingTitle = Nothing }, Cmd.batch [ editToDo id title, fetchTodos ] )
 
                 Nothing ->
-                    (model, Cmd.none)
-       
-        CancelEdit ->
-            ({ model | editingId = Nothing, editingTitle = Nothing }, Cmd.none)
-       
-        DeleteToDo id ->
-            ({ model | editingId = Nothing, editingTitle = Nothing }, Cmd.batch [ deleteToDo id, fetchTodos ])
-        
-        CompleteToDo id completed ->
-            (model, completeToDo id completed)
+                    ( model, Cmd.none )
 
-        NoOp ->
-            (model, Cmd.none)
+        CancelEdit ->
+            ( { model | editingId = Nothing, editingTitle = Nothing }, Cmd.none )
+
+        DeleteToDo id ->
+            ( { model | editingId = Nothing, editingTitle = Nothing }, Cmd.batch [ deleteToDo id, fetchTodos ] )
+
+        CompleteToDo id completed ->
+            ( model, completeToDo id completed )
 
         FetchTodos ->
-            (model, fetchTodos)
+            ( model, fetchTodos )
 
         HttpError error ->
-            ({ model | error = Just (errorToString error) }, Cmd.none)
+            ( { model | error = Just (errorToString error) }, Cmd.none )
+
+
 
 -- VIEW
+
 
 view : Model -> Html Msg
 view model =
     div []
-        [ case model.error of
-              Nothing ->
-                  text ""
+        [ h1 [] [ text "My To Do App" ]
+        , viewAddToDo model
+        ]
 
-              Just error ->
-                  div [] [ text ("Error: " ++ error) ]
-        , input [ placeholder "What needs to be done?", onInput NewInput, value model.input ] []
+
+viewAddToDo : Model -> Html Msg
+viewAddToDo model =
+    div []
+        [ case model.error of
+            Nothing ->
+                text ""
+
+            Just error ->
+                div [] [ text ("Error: " ++ error) ]
+        , input [ type_ "text", placeholder "What needs to be done?", onInput NewInput, value model.input ] []
         , button [ onClick CreateToDo, disabled (String.isEmpty model.input) ] [ text "Add" ]
         , ul [] (List.map (viewToDo model) model.todos)
         ]
+
+
+viewToDoListItem : ToDo -> Html Msg
+viewToDoListItem todo =
+    li []
+        [ input [ type_ "checkbox", checked todo.completed, onClick (CompleteToDo todo.id (not todo.completed)) ] []
+        , p [] [ text todo.title ]
+        , button [ onClick (EditToDo todo.id) ] [ text "Edit" ]
+        , button [ onClick (DeleteToDo todo.id) ] [ text "Delete" ]
+        ]
+
+
+viewEditToDo : ToDo -> String -> Html Msg
+viewEditToDo todo title =
+    li []
+        [ input [ type_ "checkbox", checked todo.completed, onClick (CompleteToDo todo.id (not todo.completed)) ] []
+        , input [ type_ "text", onInput NewTitle, value title ] []
+        , button [ onClick (SaveEdit todo.id) ] [ text "Save" ]
+        , button [ onClick CancelEdit ] [ text "Cancel" ]
+        ]
+
 
 viewToDo : Model -> ToDo -> Html Msg
 viewToDo model todo =
@@ -149,33 +196,21 @@ viewToDo model todo =
             if id == todo.id then
                 case model.editingTitle of
                     Just title ->
-                        div []
-                            [ input [ value title, onInput NewTitle ] []
-                            , button [ onClick (SaveEdit todo.id), disabled (String.isEmpty title) ] [ text "Save" ]
-                            , button [ onClick CancelEdit ] [ text "Cancel" ]
-                            ]
+                        viewEditToDo todo title
 
                     Nothing ->
                         text ""
 
             else
-                li []
-                    [ input [ type_ "checkbox", checked todo.completed, onClick (CompleteToDo todo.id (not todo.completed)) ] []
-                    , text todo.title
-                    , button [ onClick (EditToDo todo.id) ] [ text "Edit" ]
-                    , button [ onClick (DeleteToDo todo.id) ] [ text "Delete" ]
-                    ]
+                viewToDoListItem todo
 
         Nothing ->
-            li []
-                [ input [ type_ "checkbox", checked todo.completed, onClick (CompleteToDo todo.id (not todo.completed)) ] []
-                , text todo.title
-                , button [ onClick (EditToDo todo.id) ] [ text "Edit" ]
-                , button [ onClick (DeleteToDo todo.id) ] [ text "Delete" ]
-                ]
+            viewToDoListItem todo
+
 
 
 -- HTTP
+
 
 todoDecoder : Decode.Decoder ToDo
 todoDecoder =
@@ -184,59 +219,67 @@ todoDecoder =
         (Decode.field "title" Decode.string)
         (Decode.field "completed" Decode.bool)
 
+
 todoEncoder : ToDo -> Encode.Value
 todoEncoder todo =
     Encode.object
-        [ ("id", Encode.string todo.id)
-        , ("title", Encode.string todo.title)
-        , ("completed", Encode.bool todo.completed)
+        [ ( "id", Encode.string todo.id )
+        , ( "title", Encode.string todo.title )
+        , ( "completed", Encode.bool todo.completed )
         ]
+
 
 fetchTodos : Cmd Msg
 fetchTodos =
     Http.get
-        { url = "http://localhost:9228/todos"
+        { url = todosUrl
         , expect = Http.expectJson GotTodos (Decode.list todoDecoder)
         }
+
 
 createToDo : String -> Cmd Msg
 createToDo title =
     Http.request
         { method = "POST"
-        , headers = [ ]
-        , url = "http://localhost:9228/todos"
-        , body = Http.jsonBody <| Encode.object 
-            [ ("title", Encode.string title)
-            , ("completed", Encode.bool False)
-            ]
+        , headers = []
+        , url = todosUrl
+        , body =
+            Http.jsonBody <|
+                Encode.object
+                    [ ( "title", Encode.string title )
+                    , ( "completed", Encode.bool False )
+                    ]
         , expect = Http.expectWhatever (always FetchTodos)
         , timeout = Nothing
         , tracker = Nothing
         }
+
 
 editToDo : String -> String -> Cmd Msg
 editToDo id title =
     Http.request
         { method = "PATCH"
-        , headers = [ ]
+        , headers = []
         , url = "http://localhost:9228/todos/" ++ id
-        , body = Http.jsonBody (Encode.object [("title", Encode.string title)])
+        , body = Http.jsonBody (Encode.object [ ( "title", Encode.string title ) ])
         , expect = Http.expectWhatever (always FetchTodos)
         , timeout = Nothing
         , tracker = Nothing
         }
 
+
 completeToDo : String -> Bool -> Cmd Msg
 completeToDo id completed =
     Http.request
         { method = "PATCH"
-        , headers = [ ]
+        , headers = []
         , url = "http://localhost:9228/todos/" ++ id
-        , body = Http.jsonBody (Encode.object [("completed", Encode.bool completed)])
+        , body = Http.jsonBody (Encode.object [ ( "completed", Encode.bool completed ) ])
         , expect = Http.expectWhatever (always FetchTodos)
         , timeout = Nothing
         , tracker = Nothing
         }
+
 
 deleteToDo : String -> Cmd Msg
 deleteToDo id =
@@ -250,12 +293,15 @@ deleteToDo id =
         , tracker = Nothing
         }
 
+
+
 -- MAIN
+
 
 main : Program () Model Msg
 main =
     Browser.element
-        { init = always (init, fetchTodos)
+        { init = always ( init, fetchTodos )
         , update = update
         , view = view
         , subscriptions = always Sub.none
